c673d76332a0d29b5772ad382b47a806884f715b
Warble <o.katrycz@gmail.com>
2018-02-21 23:11:47 +0100

    Delete trash(3)

--- getter/src/main/java/eu/warble/getter/model/GetterFile.kt
+++ getter/src/main/java/eu/warble/getter/model/GetterFile.kt
@@ -1,8 +1,6 @@
 package eu.warble.getter.model
 
 import com.jcraft.jsch.ChannelSftp
-import net.schmizz.sshj.xfer.FilePermission
-import java.io.File
 
 data class GetterFile(var name: String, var path: String, var permissions: String,
                       var lsEntry: ChannelSftp.LsEntry){

5e87d83acdfe01a60a3045237021bf79080fd85e
Oleksandr Katrych <o.katrycz@gmail.com>
2019-06-02 22:29:10 +0200

    Update dependency Add time access function
    Signed-off-by: Oleksandr Katrych <o.katrycz@gmail.com>

--- getter/src/main/java/eu/warble/getter/model/GetterFile.kt
+++ getter/src/main/java/eu/warble/getter/model/GetterFile.kt
@@ -2,7 +2,10 @@ package eu.warble.getter.model
 
 import com.jcraft.jsch.ChannelSftp
 
-data class GetterFile(var name: String, var path: String, var permissions: String,
-                      var lsEntry: ChannelSftp.LsEntry){
+data class GetterFile(
+    var name: String, var path: String, var permissions: String,
+    var lsEntry: ChannelSftp.LsEntry
+) {
     fun isDirectory() = lsEntry.attrs.isDir
+    fun time() = lsEntry.attrs.mtimeString
 }
